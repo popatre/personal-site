a9bf9f05f0a05f436ad8e7f3b604de9867ed513b
popatre <76402245+popatre@users.noreply.github.com>
2021-09-18 18:52:26 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,64 @@
+const menuBtn = document.querySelector('.menu-btn');
+const hamburger = document.querySelector('.menu-btn__burger');
+
+const nav = document.querySelector('.nav');
+const menuNav = document.querySelector('.menu-nav');
+
+const navItems = document.querySelectorAll('.menu-nav__item');
+
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu(){
+    if(!showMenu) {
+            hamburger.classList.add('open');
+            menuNav.classList.add('open');
+            nav.classList.add('open');
+            navItems.forEach(item => item.classList.add('open'));
+            showMenu = true;
+
+    } else {
+        hamburger.classList.remove('open');
+        menuNav.classList.remove('open');
+        nav.classList.remove('open');
+        navItems.forEach(item => item.classList.remove('open'));
+        showMenu = false;
+    }
+}
+
+// Nav bar off screen when scrolling
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.querySelector(".nav").style.top = "0";
+  } else {
+    document.querySelector(".nav").style.top = "-75px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+
+anime({
+  targets: '#tester path',
+  strokeDashoffset: [anime.setDashoffset, 0],
+  easing: 'easeInElastic(1, 0.9)',
+  duration: 2500,
+  delay: function(el, i) { return i * 200 },
+  direction: 'alternate',
+  loop: false
+});
+
+//  timeout for name animation 
+
+setTimeout(() => {
+  let aaa = document.querySelector('#tester');
+aaa.classList.add('add');
+}, 9000);
+
+setTimeout(() => {
+  let aaa = document.querySelector('#tester');
+aaa.classList.add('addtwo');
+}, 9000);
