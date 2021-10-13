e208509dfe2446193917e46546cb6d239a375e6e
popatre <76402245+popatre@users.noreply.github.com>
2021-10-13 18:53:11 +0100

    changes background gradients and adds animation to hero and projects

--- js/main.js
+++ js/main.js
@@ -1,59 +1,54 @@
-const menuBtn = document.querySelector('.menu-btn');
-const hamburger = document.querySelector('.menu-btn__burger');
+const menuBtn = document.querySelector(".menu-btn");
+const hamburger = document.querySelector(".menu-btn__burger");
 
-const nav = document.querySelector('.nav');
-const menuNav = document.querySelector('.menu-nav');
+const nav = document.querySelector(".nav");
+const menuNav = document.querySelector(".menu-nav");
 
-const navItems = document.querySelectorAll('.menu-nav__item');
+const navItems = document.querySelectorAll(".menu-nav__item");
 
 let showMenu = false;
 
-menuBtn.addEventListener('click', toggleMenu);
+menuBtn.addEventListener("click", toggleMenu);
 
-function toggleMenu(){
-    if(!showMenu) {
-            hamburger.classList.add('open');
-            menuNav.classList.add('open');
-            nav.classList.add('open');
-            navItems.forEach(item => item.classList.add('open'));
-            showMenu = true;
-
-    } else {
-        hamburger.classList.remove('open');
-        menuNav.classList.remove('open');
-        nav.classList.remove('open');
-        navItems.forEach(item => item.classList.remove('open'));
-        showMenu = false;
-    }
+function toggleMenu() {
+  if (!showMenu) {
+    hamburger.classList.add("open");
+    menuNav.classList.add("open");
+    nav.classList.add("open");
+    navItems.forEach((item) => item.classList.add("open"));
+    showMenu = true;
+  } else {
+    hamburger.classList.remove("open");
+    menuNav.classList.remove("open");
+    nav.classList.remove("open");
+    navItems.forEach((item) => item.classList.remove("open"));
+    showMenu = false;
+  }
 }
 
 // Nav bar off screen when scrolling
 
 var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
+window.onscroll = function () {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.querySelector(".nav").style.top = "0";
   } else {
-    
     document.querySelector(".nav").style.top = "-75px";
   }
   prevScrollpos = currentScrollPos;
-}
+};
 
+let projectSlider = document.querySelectorAll(".projects__item");
 
+projectSlider.forEach((a) =>
+  a.addEventListener("click", function () {
+    this.classList.toggle("slider");
+    this.classList.toggle("special");
 
-let projectSlider = document.querySelectorAll(".projects__item")
-
-projectSlider.forEach(a => a.addEventListener('click', function(){
-  this.classList.add("slider");
-  this.classList.add("special");
-  
-  setTimeout(() => {
-    this.classList.remove("special");
-    this.classList.add("slider");
-  }, 4000);
-
-}) ); 
-
-
+    // setTimeout(() => {
+    //   this.classList.remove("special");
+    //   this.classList.add("slider");
+    // }, 4000);
+  })
+);
