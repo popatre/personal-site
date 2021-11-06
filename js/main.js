09e3e0f07a7ebba2804ceb05943f90cf995d909e
popatre <76402245+popatre@users.noreply.github.com>
2021-11-06 17:45:09 +0000

    fixes hero animation and changes menu bar to scroll with page, instead of fixed

--- js/main.js
+++ js/main.js
@@ -11,44 +11,44 @@ let showMenu = false;
 menuBtn.addEventListener("click", toggleMenu);
 
 function toggleMenu() {
-  if (!showMenu) {
-    hamburger.classList.add("open");
-    menuNav.classList.add("open");
-    nav.classList.add("open");
-    navItems.forEach((item) => item.classList.add("open"));
-    showMenu = true;
-  } else {
-    hamburger.classList.remove("open");
-    menuNav.classList.remove("open");
-    nav.classList.remove("open");
-    navItems.forEach((item) => item.classList.remove("open"));
-    showMenu = false;
-  }
+    if (!showMenu) {
+        hamburger.classList.add("open");
+        menuNav.classList.add("open");
+        nav.classList.add("open");
+        navItems.forEach((item) => item.classList.add("open"));
+        showMenu = true;
+    } else {
+        hamburger.classList.remove("open");
+        menuNav.classList.remove("open");
+        nav.classList.remove("open");
+        navItems.forEach((item) => item.classList.remove("open"));
+        showMenu = false;
+    }
 }
 
 // Nav bar off screen when scrolling
 
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function () {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.querySelector(".nav").style.top = "0";
-  } else {
-    document.querySelector(".nav").style.top = "-75px";
-  }
-  prevScrollpos = currentScrollPos;
-};
+// var prevScrollpos = window.pageYOffset;
+// window.onscroll = function () {
+//   var currentScrollPos = window.pageYOffset;
+//   if (prevScrollpos > currentScrollPos) {
+//     document.querySelector(".nav").style.top = "0";
+//   } else {
+//     document.querySelector(".nav").style.top = "-75px";
+//   }
+//   prevScrollpos = currentScrollPos;
+// };
 
 let projectSlider = document.querySelectorAll(".projects__item");
 
 projectSlider.forEach((a) =>
-  a.addEventListener("click", function () {
-    this.classList.toggle("slider");
-    this.classList.toggle("special");
+    a.addEventListener("click", function () {
+        this.classList.toggle("slider");
+        this.classList.toggle("special");
 
-    // setTimeout(() => {
-    //   this.classList.remove("special");
-    //   this.classList.add("slider");
-    // }, 4000);
-  })
+        // setTimeout(() => {
+        //   this.classList.remove("special");
+        //   this.classList.add("slider");
+        // }, 4000);
+    })
 );
