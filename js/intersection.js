e5286e8401eab6a701bfe98498286a89bb2b3c8b
popatre <76402245+popatre@users.noreply.github.com>
2022-12-06 11:06:40 +0000

    add fade jobs to about me

---
+++ js/intersection.js
@@ -0,0 +1,28 @@
+/* intersection Observer */
+
+const faders = document.querySelectorAll(".fade-in");
+
+const appearOptions = {
+    // root: null,
+    threshold: 0.5,
+    rootMargin: "0px 0px -100px 0px",
+};
+
+const appearOnScroll = new IntersectionObserver(function (
+    entries,
+    appearOnScroll
+) {
+    entries.forEach((entry) => {
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            entry.target.classList.add("appear");
+            appearOnScroll.unobserve(entry.target);
+        }
+    });
+},
+appearOptions);
+
+faders.forEach((fader) => {
+    appearOnScroll.observe(fader);
+});
